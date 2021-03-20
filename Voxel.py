81078b6c9f70bc35380ab91f0f5a63767d61a225
PLATnya <platinumcool777@gmail.com>
2021-03-20 20:21:35 +0200

    voxel start setup

--- Voxel.py
+++ Voxel.py
@@ -1,12 +1,7 @@
+import numpy as np
+
 class Voxel:
-    def __init__(self, voxelSize):
-        self.IsActive = false
+    def __init__(self):
+        self.IsActive = True
         self.localPosition = (0,0,0)
 
-        wireCube(voxelSize)
-    def wireCube(voxelSize):
-        cubeEdges = np.array(((0,1),(0,3),(0,4),(1,2),(1,7),(2,5),(2,3),(3,6),(4,6),(4,7),(5,6),(5,7)))
-
-        for cubeEdge in cubeEdges:
-            for cubeVertex in cubeEdge:
-                glVertex3fv(cubeVertices[cubeVertex]*voxelSize)
