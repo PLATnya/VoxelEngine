a82471227a756dfc369b4fac154fbde53ad22932
PLATnya <platinumcool777@gmail.com>
2021-03-21 20:25:35 +0200

    left right arrow keydown event

--- Engine.py
+++ Engine.py
@@ -20,8 +20,9 @@ class Event:
         self.event_type = event_type
     def action(self):
         pass
-    def onNotify(self, event_type):
-        if self.event_type == event_type:
+    def onNotify(self, event):
+        self.event = event
+        if self.event_type == event.type:
             self.action()
 
 class CloseEvent(Event):
@@ -32,12 +33,25 @@ class CloseEvent(Event):
         pg.quit()
         sys.exit()
 
+
+class KeyDownEvent(Event):
+    def __init__ (self):
+        super().__init__(pg.KEYDOWN)
+    def action(self):
+        self.key = self.event.key
+        if(self.key == pg.K_LEFT):
+            glTranslatef(-0.1,0,0)
+        elif(self.key == pg.K_RIGHT):
+            glTranslate(0.1,0,0)
+
+
+
 class EventHandler:
     def __init__ (self):
         self.events = []
-    def notify(self,event_type):
+    def notify(self,event_in):
         for event in self.events:
-            event.onNotify(event_type)
+            event.onNotify(event_in)
 
 
 
