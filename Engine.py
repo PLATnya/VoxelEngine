c4ea0f84ee461e231c106f7e90a4c32f6eeeced0
PLATnya <platinumcool777@gmail.com>
2021-03-24 22:11:10 +0200

    graphics engine start setup

--- Engine.py
+++ Engine.py
@@ -1,33 +1,49 @@
-
 import sys
 
-
 from GraphicsEngine import *
+
+from Chunk import Chunk
+
 
 class ChunkManager:
     def __init__(self):
         self.chunks = []
+
     def createChunk(self):
         chunk = Chunk()
         self.chunks.append(chunk)
         return chunk
+
     def renderAll(self):
         for chunk in self.chunks:
             renderChunk(chunk)
 
+
+class ActorManager:
+    def __init__(self):
+        self.actors = []
+
+    def addActor(self, actor):
+        self.actors.append(actor)
+
+
 class Event:
     def __init__(self, event_type):
         self.event_type = event_type
+
     def action(self):
         pass
+
     def onNotify(self, event):
         self.event = event
         if self.event_type == event.type:
             self.action()
 
+
 class CloseEvent(Event):
-    def __init__ (self):
+    def __init__(self):
         super().__init__(pg.QUIT)
+
     def action(self):
         exit()
         pg.quit()
@@ -35,23 +51,21 @@ class CloseEvent(Event):
 
 
 class KeyDownEvent(Event):
-    def __init__ (self):
+    def __init__(self):
         super().__init__(pg.KEYDOWN)
+
     def action(self):
         self.key = self.event.key
-        if(self.key == pg.K_LEFT):
-            glTranslatef(-0.1,0,0)
-        elif(self.key == pg.K_RIGHT):
-            glTranslate(0.1,0,0)
-
+        if (self.key == pg.K_LEFT):
+            glTranslatef(-0.1, 0, 0)
+        elif (self.key == pg.K_RIGHT):
+            glTranslate(0.1, 0, 0)
 
 
 class EventHandler:
-    def __init__ (self):
+    def __init__(self):
         self.events = []
-    def notify(self,event_in):
+
+    def notify(self, event_in):
         for event in self.events:
             event.onNotify(event_in)
-
-
-
---
+++ GraphicsEngine.py
@@ -0,0 +1,43 @@
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import numpy as np
+import pygame as pg
+
+VOXEL_SIZE = 1
+
+
+def GraphicSetup():
+    pg.init()
+    display = (600, 400)
+    pg.display.set_mode(display, DOUBLEBUF | OPENGL)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+
+    gluLookAt(0, 0, 0, 0, 0, 5, 0, 100, 0)
+
+
+def clearScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+
+def createWireCube(active, pivot):
+    if active:
+        cube_vertices = np.array(
+            ((1, 1, 1), (1, 1, -1), (1, -1, -1), (1, -1, 1), (-1, 1, 1), (-1, -1, -1), (-1, -1, 1), (-1, 1, -1)))
+        cube_edges = np.array(
+            ((0, 1), (0, 3), (0, 4), (1, 2), (1, 7), (2, 5), (2, 3), (3, 6), (4, 6), (4, 7), (5, 6), (5, 7)))
+        global VOXEL_SIZE
+        for cubeEdge in cube_edges:
+            for cubeVertex in cubeEdge:
+                position = tuple(np.array(cube_vertices[cubeVertex]) * VOXEL_SIZE + pivot)
+                glVertex3fv(position)
+
+
+def renderChunk(chunk):
+    glBegin(GL_LINES)
+    for voxel in chunk.voxels:
+        createWireCube(True, np.array(voxel.parent.worldPosition) + np.array(voxel.localPosition))
+    glEnd()
