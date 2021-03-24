386360a114888974e96fe35e69a6c3aa7f29194d
PLATnya <platinumcool777@gmail.com>
2021-03-24 21:37:19 +0200

    dependence voxel on actor and chunk(decomposition)

--- Voxel.py
+++ Voxel.py
@@ -1,7 +1,17 @@
-import numpy as np
+from Engine import ChunkManager, Actor
+
 
 class Voxel:
-    def __init__(self):
+    def __init__(self, chunk_manager: ChunkManager, actor: Actor):
         self.IsActive = True
-        self.localPosition = (0,0,0)
+        self.localPosition = (0, 0, 0)
 
+        # add voxel to chunk for rendering
+        if chunk_manager.chunks[-1].isFull:
+            chunk = chunk_manager.createChunk()
+            chunk.addVoxel(self)
+        else:
+            chunk_manager.chunks[-1].append(self)
+
+        # add voxel to actor for transformations
+        actor.voxels.append(self)
