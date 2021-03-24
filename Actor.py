1e6f05d3a3ca148092e6b9f77ade7abef369e209
PLATnya <platinumcool777@gmail.com>
2021-03-24 21:42:31 +0200

    actor size property

--- Actor.py
+++ Actor.py
@@ -1,10 +1,13 @@
 class Actor:
     def __init__(self):
         self.voxels = []
-        self.startBehaviur = None
+        self.startBehaviour = None
         self.endBehaviour = None
         self.tickBehaviour = None
-        self.worldPosition = (0,0,0)
+        self.worldPosition = (0, 0, 0)
+
+    @property
     def size(self):
         return len(self.voxels)
+
 
