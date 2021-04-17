248f9e51c6acea13bbe148a633bf080643a22e36
PLATnya <vadim.plataniy@gmail.com>
2021-04-17 13:47:47 +0300

    voxel on grid

--- Voxel.py
+++ Voxel.py
@@ -1,9 +1,11 @@
-from Engine import ChunkManager,np,VOXEL_SIZE
+from Engine import ChunkManager, np, VOXEL_SIZE
 from Actor import Actor
 import Session
 import Session
+
+
 class Voxel:
-    def __init__(self, color, actor = None, position = (0,0,0)):
+    def __init__(self, color, actor=None, position=(0, 0, 0)):
         chunk_manager = Session.GameSession().chunk_manager
         self.IsActive = True
 
@@ -22,12 +24,19 @@ class Voxel:
             self.parent = actor
         self.SetPosition(*position)
 
-    def SetPosition(self, x,y,z):
-        #TODO: grid position
-
-        if self.parent!=None:
-            self.localPosition = np.array([x,y,z])
+    def SetPosition(self, gridX, gridY, gridZ):
+        if Session.GameSession().matrix[gridX, gridY, gridZ] == None:
+            Session.GameSession().matrix[gridX, gridY, gridZ] = self
+        gridFactor = VOXEL_SIZE * 2
+        x = gridX * gridFactor
+        y = gridY * gridFactor
+        z = gridZ * gridFactor
+        if self.parent != None:
+            self.localPosition = np.array([x, y, z])
             self.globalPosition = np.array(self.parent.worldPosition) + np.array(self.localPosition)
         else:
-            self.localPosition = np.array([0,0,0])
-            self.globalPosition = np.array([x,y,z])
+            self.localPosition = np.array([0, 0, 0])
+            self.globalPosition = np.array([x, y, z])
+
+    def GetPosition(self):
+        return
