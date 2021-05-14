24b24961dd88625f1e1ffa5090b41e616bedc5c8
V <platinumcool777@gmail.com>
2021-05-14 20:48:42 +0300

    Make voxel buffering in started game

--- Session.py
+++ Session.py
@@ -1,8 +1,9 @@
 from enum import Enum
-from GraphicsEngine import pg, clear_screen,graphic_setup
+from GraphicsEngine import pg, clear_screen, graphic_setup
 from Engine import EventHandler, ChunkManager, ActorManager
 from Engine import PressEndEvent, PressStartEvent, PressRestartEvent
 import numpy as np
+
 MATRIX_SIZE = 100
 
 
@@ -29,7 +30,7 @@ class GameSessionMeta(type):
             cls.chunk_manager = ChunkManager()
             cls.actor_manager = ActorManager()
             from Voxel import Voxel
-            cls.construct_voxel = Voxel((0, 1, 0))
+            cls.construct_voxel = Voxel((0, 1, 0), is_indexed_by_matrix=False)
             cls.restart_event = PressRestartEvent(cls.pressed_buffer)
             cls.start_event = PressStartEvent(cls.pressed_buffer)
             cls.end_event = PressEndEvent(cls.pressed_buffer)
@@ -53,24 +54,26 @@ class GameSession(metaclass=GameSessionMeta):
             pg.display.flip()
 
     def change_state(self, new_state):
-        if new_state == GameSessionState.LIFE:
+        if new_state == GameSessionState.PRELIFE:
+            self.matrix_field.clear_instanced_buff()
+            self.construct_voxel.isActive = True
+            self.matrix_field.load_buff()
+            self.events_handler.remove_event(self.restart_event)
+            self.events_handler.add_event(self.start_event)
 
+        elif new_state == GameSessionState.LIFE:
             self.construct_voxel.isActive = False
             self.matrix_field.save_buff()
             self.events_handler.remove_event(self.start_event)
             self.events_handler.add_event(self.end_event)
             # TODO: start main life looop
-        elif new_state == GameSessionState.PRELIFE:
-            self.construct_voxel.isActive = True
-            self.matrix_field.load_buff()
-            self.events_handler.remove_event(self.restart_event)
-            self.events_handler.add_event(self.start_event)
 
         elif new_state == GameSessionState.AFTERLIFE:
             self.construct_voxel.isActive = False
             # add restart button or event
             self.events_handler.add_event(self.restart_event)
             self.events_handler.remove_event(self.end_event)
+
         self.state = new_state
         print(self.state)
 
@@ -78,9 +81,8 @@ class GameSession(metaclass=GameSessionMeta):
 class MatrixField:
     def __init__(self):
         self.matrix = np.empty((MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, 2), dtype=object)
-        self.clear_matrix()
-        self.buff_indexes = None
-        self.buff_positions = None
+        self.buff_indexes = np.array([])
+        self.buff_instanced_indexes = []
 
     def __getitem__(self, item):
         return self.matrix[item]
@@ -90,21 +92,23 @@ class MatrixField:
 
     def save_buff(self):
         self.buff_indexes = np.argwhere(np.invert(self.matrix[:, :, :, 0] == None))
-        self.buff_positions = self.matrix[self.buff_indexes[:, 0], self.buff_indexes[:, 1], self.buff_indexes[:, 2], 0]
-        self.buff_positions = [i.globalPosition for i in self.buff_positions]
 
     def load_buff(self):
-        if self.buff_indexes:
+        if self.buff_indexes.size > 0:
             for n, index in enumerate(self.buff_indexes):
                 from Voxel import Voxel
-                self.matrix[index[0], index[1], index[2]] = Voxel((1, 0, 1), position=self.buff_positions[n])
+                self.matrix[index[0], index[1], index[2], 0] = Voxel((1, 0, 1), position=index)
 
     def is_empty(self):
         voxels_matrix = self.matrix[:, :, :, 0]
-        return (np.sum(voxels_matrix == None)/np.size(voxels_matrix)) == 1.0
+        return (np.sum(voxels_matrix == None) / np.size(voxels_matrix)) == 1.0
 
-    def clear_matrix(self):
-        # TODO: matrix delete voxels on restart
-        self.matrix[:, :, :, 0] = None
-        self.matrix[:, :, :, 1] = 0
+    def clear_instanced_buff(self):
+        if len(self.buff_instanced_indexes) > 0:
+            for n, index in enumerate(self.buff_instanced_indexes):
+                from Voxel import Voxel
+                self.matrix[index[0], index[1], index[2], 0].delete_from_chunk()
+                self.matrix[index[0], index[1], index[2], 0] = None
+        self.buff_instanced_indexes = []
 
+        # # TODO: matrix delete voxels on restart
